b71012924cca9ad6eecd17d1f682458b1b8f83f2
taohui <htao@thoughtworks.com>
2019-07-11 17:33:07 +0800

    [taohui] modify eureka dependency to make consumer work

--- src/main/java/com/cloud/spring/ribbon/consumer/ConsumerController.java
+++ src/main/java/com/cloud/spring/ribbon/consumer/ConsumerController.java
@@ -1,6 +1,8 @@
 package com.cloud.spring.ribbon.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -8,11 +10,16 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ConsumerController {
 
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
     @Autowired
     RestTemplate restTemplate;
 
     @GetMapping("/ribbon-consumer")
     public String helloConsumer(){
-        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+        return restTemplate.getForObject("http://HELLO-SERVICE/hello", String.class);
     }
 }
